a4ea9bfc6ba8db63489745c9a626ffd42ca672f9
Oksana-dnp <ksana.kharchenko@gmail.com>
2022-10-21 21:17:30 +0300

    version_1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
     let items="";
     let counter=0;
-    for(let i=0, y=0; i<4; i++,y-=84){
+    for(let i=0, y=0; i<4; i++,y-=125){
         counter++
-       for(let j=0, x=0; j<4; j++, x-=110){
-        console.log(`${counter}`, `<div style="background-position: ${x}px ${y}px"></div>`)
-
-        items+=`<div style="background-position: ${x}px ${y}px"></div>`
+       for(let j=0, x=0; j<4; j++, x-=125){
+          items+=`<div style="background-position: ${x}px ${y}px"></div>`
        }
-       $('.puzzle').html(items);
+       $('.game__puzzle-box').html(items);
     }
-    
+    $( ".sort" ).sortable({
+        connectWith: '.game__puzzle-box, .game__place-box'
+    })
+   /*  $( ".game__puzzle-box div" ).draggable({
+        containment: ".game__place-box",
+        distance: 10,
+    })
+    $('.game__puzzle-box div').on('mouseup', function(){
+        let postionTop = parseInt($(this).css('top'));
+        let postionLeft = parseInt($(this).css('left'));
+        if(postionTop != 0 || postionLeft != 0 ) {
+            $(this).css('margin', '0px');
+        }
+        console.log('ddd',postionTop)
+        console.log('ddd',postionLeft)
+    }) */
+
 
 })
