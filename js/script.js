54656a2f45d22b0c4b9ef06f05f24643655cf60a
Oksana-dnp <71383222+Oksana-dnp@users.noreply.github.com>
2022-11-01 20:32:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,14 @@
 $(document).ready(function () {
   let items = "";
   let counter = 0;
-
-
- 
-  function puzzleGenerate() { 
   $(".info-win").fadeOut();
   $(".modal__check").fadeOut();
-  $(".modal__check").fadeOut();
   $(".info-lose").fadeOut();
-    if(counter !=0 ) counter=0;
-    if(items) items = "";
+  function puzzleGenerate() {
+    $(".game__puzzle-box").html("");
+    $(".game__place-box").html("");
+    if (counter != 0) counter = 0;
+    if (items) items = "";
     for (let i = 0, y = 0; i < 4; i++, y -= 125) {
       for (let j = 0, x = 0; j < 4; j++, x -= 125) {
         counter++;
@@ -26,44 +24,74 @@ $(document).ready(function () {
         );
       }
     }
+    $(".item").draggable({
+      containment: ".game__place",
+      grid: [125, 125],
+    });
   }
- puzzleGenerate();
+
+  let array = [];
+
+  puzzleGenerate();
   let min, sec;
   let secId;
+
   let timerStart = false;
   $("#check-result").prop("disabled", true);
-  $("#start").on("click", startTimer)
-  $('.item').draggable({
-    containment: ".game__place",
-    grid: [125, 125]
-  })
-  $('#game__place').droppable({
-    accept: '.item',    
-    drop: function(){
-      if(timerStart === false){
-         startTimer();
-         timerStart = true;
+  $("#start").on("click", startTimer);
+
+  $("#game__place").droppable({
+    accept: ".item",
+    drop: function (event, ui) {
+      let item = {};
+      item.top = ui.offset.top;
+      item.left = ui.offset.left;
+      item.index = ui.draggable.data("index");
+      $(".item").on("dragstart", function () {
+        console.log("left", ui.offset.left);
+        item.top = ui.offset.top;
+        item.left = ui.offset.left;
+      });
+      let check = true;
+      if (array.length > 0) {
+        for (let elem of array) {
+          if (elem.index == item.index) {
+            elem.top = item.top;
+            elem.left = item.left;
+            check = false;
+            if (check == false) console.log(elem.left, item.left);
+          }
+        }
       }
-    }
-  })
+      if (check) array.push(item);
+      console.log('timerstart-->', timerStart)
+      if (timerStart === false && $("#start").prop("disabled", false)) {
+        timerStart = true;
+        startTimer();
+      }
+    },
+  });
 
   //timer functions
-  function startTimer(){
+  function startTimer() {
     $("#start").prop("disabled", true);
     $("#check-result").prop("disabled", false);
     min = "00";
     sec = 60;
     secId = setInterval(() => {
       sec--;
-      if (sec == 0) {
-        showLoseMessage();
+      if (sec === 0) {
+        console.log("sec", sec);
         timerStart = false;
         clearInterval(secId);
+        showLoseMessage();
       }
       sec < 10
         ? $("#timer").text(`${min}:0${sec}`)
         : $("#timer").text(`${min}:${sec}`);
-        sec < 10 ? $(".info").text(`Your still have time? you sure? ${min}:0${sec}`) : $(".info").text(`Your still have time? you sure? ${min}:${sec}`);
+      sec < 10
+        ? $(".info").text(`Your still have time? you sure? ${min}:0${sec}`)
+        : $(".info").text(`Your still have time? you sure? ${min}:${sec}`);
     }, 1000);
   }
   function stopTimer() {
@@ -82,49 +110,54 @@ $(document).ready(function () {
     $(".modal__check").fadeIn();
   });
   $("#close").on("click", function () {
+    $(".modal-window").css("z-index", "-1");
     $(".modal__check").fadeOut();
+  });
+
+  $(".btn-close").on("click", function () {
+    if ($(this).data("name") == "lose") $(".info-lose").fadeOut();
+    if ($(this).data("name") == "win") $(".info-win").fadeOut();
     $(".modal-window").css("z-index", "-1");
   });
 
-  $('.btn-close').on('click', function(){
-    if($(this).data('name') == 'lose')  $(".info-lose").fadeOut();
-    if($(this).data('name') == 'win') $(".info-win").fadeOut();
-    $(".modal-window").css("z-index", "-1");
-  })
-
   $("#check").on("click", function () {
     clearInterval(secId);
-    for (let i = 0; i < $(".item").length; i++) {
-      if ($(".game__place-box div").eq(i).data("index") != winArray[i]) {
+    $("#check-result").prop("disabled", true);
+    timerStart = false;
+    array.sort((a, b) => {
+      if (a.top === b.top) {
+        return parseInt(a.left) - parseInt(b.left);
+      }
+      return parseInt(a.top) - parseInt(b.top);
+    });
+    checkArray = array.map((key) => key.index);
+    $(".modal__check").fadeOut();
+    for (let i = 0; i < array.length; i++) {
+      if (checkArray[i] != winArray[i]) {
         check = false;
         break;
       }
     }
-
     if (check) {
-      showWinMessage()
+      showWinMessage();
     } else {
-      showLoseMessage()
+      showLoseMessage();
     }
     check = true;
   });
 
-  function showWinMessage(){
+  function showWinMessage() {
     $(".modal-window").css("z-index", "3");
-    $(".info-win").fadeIn();
+    $(".info-win").fadeIn("slow");
   }
-  function showLoseMessage(){
+  function showLoseMessage() {
     $(".modal-window").css("z-index", "3");
-    $(".info-lose").fadeIn();
-    $("#check-result").prop("disabled", true);
+    $(".info-lose").fadeIn("slow");
   }
 
   $("#new-game").on("click", function () {
-    $(".game__puzzle-box").html("");
-    $(".game__place-box").html("");
     puzzleGenerate();
     stopTimer();
     $("#start").prop("disabled", false);
   });
-
 });
